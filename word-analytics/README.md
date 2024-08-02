8b1d8c000fe0ab6a0ec1bfc055bddffcb4d9eb5f
resident7709 <resident7709@gmail.com>
2024-08-02 14:51:53 +0300

    Setup for Word_Analytics

---
+++ word-analytics/README.md
@@ -0,0 +1,6 @@
+# React + Vite
+
+WORD ANALYTICS // React modern analytics application
+
+This project introduces React fundamentals and more intermediate concepts like
+reusable component techniques and derived/computed state.
---
+++ word-analytics/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
