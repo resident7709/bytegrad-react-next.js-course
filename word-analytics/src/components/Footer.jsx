703e319347e3fbc9d7005188335d04f953834936
resident7709 <resident7709@gmail.com>
2024-08-06 16:31:59 +0300

    Finish the WordAnalytics

--- word-analytics/src/components/Footer.jsx
+++ word-analytics/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='footer'>
-      <small>&copy; Copyright & Reserved by Bytegrad</small>
+      <small>&copy; Copyright & Reserved by ByteGrad</small>
       <small>Last checked limits: 17 days ago</small>
     </footer>
   );
